190e7637cdaa08fd393b1653b1a2707abfa6e02c
Felipe Mamud <felipe.mamud@vivareal.com>
2017-08-14 01:41:50 -0300

    removing unnecessary code

--- src/main/java/com/simscale/tracer/model/LogLine.java
+++ src/main/java/com/simscale/tracer/model/LogLine.java
@@ -36,30 +36,4 @@ public class LogLine {
     public String getCallerSpan() {
         return callerSpan;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        LogLine logLine = (LogLine) o;
-
-        if (start != null ? !start.equals(logLine.start) : logLine.start != null) return false;
-        if (end != null ? !end.equals(logLine.end) : logLine.end != null) return false;
-        if (trace != null ? !trace.equals(logLine.trace) : logLine.trace != null) return false;
-        if (serviceName != null ? !serviceName.equals(logLine.serviceName) : logLine.serviceName != null) return false;
-        if (callerSpan != null ? !callerSpan.equals(logLine.callerSpan) : logLine.callerSpan != null) return false;
-        return span != null ? span.equals(logLine.span) : logLine.span == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = start != null ? start.hashCode() : 0;
-        result = 31 * result + (end != null ? end.hashCode() : 0);
-        result = 31 * result + (trace != null ? trace.hashCode() : 0);
-        result = 31 * result + (serviceName != null ? serviceName.hashCode() : 0);
-        result = 31 * result + (callerSpan != null ? callerSpan.hashCode() : 0);
-        result = 31 * result + (span != null ? span.hashCode() : 0);
-        return result;
-    }
 }
